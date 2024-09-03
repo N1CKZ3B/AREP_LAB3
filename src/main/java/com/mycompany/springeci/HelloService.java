a5cf1d36c5e93d6e8c4974f87e4d8502277b19fd
N1CKZ3V <nico_03_sebas@hotmail.com>
2024-09-03 18:48:07 -0500

    Faltan Ajustes mínimos

--- src/main/java/com/mycompany/springeci/HelloService.java
+++ src/main/java/com/mycompany/springeci/HelloService.java
@@ -1,9 +1,17 @@
 
 package com.mycompany.springeci;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 @RestController
 public class HelloService{
-    
+    private static final String STATIC_FILES_DIR = "src/main/resources";
+
+
     @GetMapping("/hello")
     public static String hello(){
         return "Hello World!";
@@ -23,8 +31,67 @@ public class HelloService{
     public static String editor(){
         return "El editor es Nicolas Sebastian Achuri Macias";
     }
-    
-    
-    
-    
+
+    @GetMapping("/greeting")
+    public static String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hola, " + name ;
+    }
+
+    @GetMapping("/")
+    public static String serveIndex() {
+        return getStaticFileContent("index.html");
+    }
+
+
+    @GetMapping("/staticfile")
+    public static String serveStaticContent(@RequestParam(value = "file", defaultValue = "index.html") String fileName) {
+        return getStaticFileContent(fileName);
+    }
+
+    static String getStaticFileContent(String fileName) {
+        File file = new File(STATIC_FILES_DIR + File.separator + fileName);
+
+        if (file.exists() && !file.isDirectory()) {
+            try {
+                String contentType = determineContentType(fileName);
+                byte[] fileContent = Files.readAllBytes(file.toPath());
+
+                if (contentType.startsWith("image")) {
+                    String base64Image = Base64.getEncoder().encodeToString(fileContent);
+                    String htmlResponse = "<!DOCTYPE html>\r\n"
+                            + "<html>\r\n"
+                            + "    <head>\r\n"
+                            + "        <title>Image</title>\r\n"
+                            + "    </head>\r\n"
+                            + "    <body>\r\n"
+                            + "        <center><img src=\"data:" + contentType + ";base64," + base64Image + "\" alt=\"image\"></center>\r\n"
+                            + "    </body>\r\n"
+                            + "</html>";
+                    return "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + htmlResponse.length() + "\r\n\r\n" + htmlResponse;
+                } else {
+                    return "HTTP/1.1 200 OK\r\nContent-Type: " + contentType + "\r\nContent-Length: " + fileContent.length + "\r\n\r\n" + new String(fileContent);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "HTTP/1.1 500 Internal Server Error\r\n\r\nError reading file";
+            }
+        } else {
+            return "HTTP/1.1 404 Not Found\r\n\r\nFile not found";
+        }
+    }
+
+    private static String determineContentType(String fileName) {
+        if (fileName.endsWith(".html")) {
+            return "text/html";
+        } else if (fileName.endsWith(".js")) {
+            return "application/javascript";
+        } else if (fileName.endsWith(".css")) {
+            return "text/css";
+        } else if (fileName.endsWith(".png")) {
+            return "image/png";
+        } else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+            return "image/jpeg";
+        }
+        return "application/octet-stream";
+    }
 }
--- src/main/java/com/mycompany/springeci/Springeci.java
+++ src/main/java/com/mycompany/springeci/Springeci.java
@@ -6,6 +6,7 @@ package com.mycompany.springeci;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
@@ -29,8 +30,59 @@ public class Springeci {
                     services.put(key,m);
                 }
             }
-        }  
-        URL serviceurl = new URL("http://localhost8080/App/hello");
+        }
+
+        URL serviceurl = new URL("http://localhost:8080/App/greeting?name=Nicolas");
+        String path = serviceurl.getPath();
+        String query = serviceurl.getQuery();
+        System.out.println("path: " + path);
+        System.out.println("query: " + query);
+
+
+        String serviceName = path.substring(4);
+        System.out.println("Service Name: " + serviceName);
+
+
+        Method ms = services.get(serviceName);
+        if (ms != null) {
+            Object[] argsToPass = extractArguments(ms, query); // Extraer parámetros de la consulta
+            System.out.println("Rta" + ms.invoke(null, argsToPass));
+        } else {
+            System.out.println("Servicio no encontrado: " + serviceName);
+        }
+    }
+
+    private static Object[] extractArguments(Method method, String query) {
+        Map<String, String> queryParams = parseQuery(query);
+        Parameter[] parameters = method.getParameters();
+        Object[] args = new Object[parameters.length];
+
+        for (int i = 0; i < parameters.length; i++) {
+            if (parameters[i].isAnnotationPresent(RequestParam.class)) {
+                RequestParam annotation = parameters[i].getAnnotation(RequestParam.class);
+                String paramName = annotation.value();
+                String value = queryParams.getOrDefault(paramName, annotation.defaultValue());
+                args[i] = value;
+            }
+        }
+        return args;
+    }
+
+    private static Map<String, String> parseQuery(String query) {
+        Map<String, String> queryParams = new HashMap<>();
+        if (query != null) {
+            String[] pairs = query.split("&");
+            for (String pair : pairs) {
+                String[] keyValue = pair.split("=");
+                if (keyValue.length > 1) {
+                    queryParams.put(keyValue[0], keyValue[1]);
+                }
+            }
+        }
+        return queryParams;
+    }
+
+       /* URL serviceurl = new URL("http://localhost8080/App/hello");
 
         String path = serviceurl.getPath();
         System.out.println(path);
@@ -73,7 +125,11 @@ public class Springeci {
         Method ms3 = services.get(serviceName3);
 
         System.out.println("Rta: "+ ms3.invoke(ms));
-    }
+
+        }
+        */
+
+
     
    
     
